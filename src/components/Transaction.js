bd01d0ec5f5b2def111653fdfb7ef151a1f045b1
Debopriyo Basu <debopriyobasu@gmail.com>
2023-12-19 00:43:28 +0530

    fix: color coding

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -4,7 +4,7 @@ const Transaction = ({ transaction }) => {
   const { deleteTransaction } = useContext(GlobalContext);
   const sign = transaction.amount > 0 ? "+" : "-";
   return (
-    <li className={transaction.amount > 0 ? "minus" : "plus"}>
+    <li className={transaction.amount > 0 ? "plus" : "minus"}>
       {transaction.text}{" "}
       <span>
         {sign} ${Math.abs(transaction.amount)}
