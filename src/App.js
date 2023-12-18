45000706574cf8b8532ef55895a5b21a1dc27b7a
Debopriyo Basu <debopriyobasu@gmail.com>
2023-12-19 00:21:24 +0530

    feat: added expense tracker

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import "./App.css";
+import Header from "./components/Header";
+import Balance from "./components/Balance";
+import IncomeExpenses from "./components/IncomeExpenses";
+import TransactionList from "./components/TransactionList";
+import AddTransaction from "./components/AddTransaction";
+import { GlobalProvider } from "./context/GlobalState";
+function App() {
+  return (
+    <GlobalProvider>
+      <Header />
+      <Balance />
+      <IncomeExpenses />
+      <TransactionList />
+      <AddTransaction />
+    </GlobalProvider>
+  );
+}
+
+export default App;
